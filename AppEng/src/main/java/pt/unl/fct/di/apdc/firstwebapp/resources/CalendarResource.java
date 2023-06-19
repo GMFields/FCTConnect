7258de2525ccb7daad634a67e92e56cd59b87752
Tiago Francisco <tr.francisco@campus.fct.unl.pt>
2023-06-19 16:01:21 +0100

    calendar endpoints

--- AppEng/src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/CalendarResource.java
+++ AppEng/src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/CalendarResource.java
@@ -1,8 +1,9 @@
 package pt.unl.fct.di.apdc.firstwebapp.resources;
 
 import com.google.cloud.datastore.*;
+
 import com.google.gson.Gson;
-import pt.unl.fct.di.apdc.firstwebapp.api.CalendarAPI;
+import pt.unl.fct.di.apdc.firstwebapp.api.CalendarApi;
 
 import pt.unl.fct.di.apdc.firstwebapp.factory.KeyStore;
 
@@ -14,12 +15,14 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 
 @Path("/calendar")
 @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-public class CalendarResource implements CalendarAPI {
+public class CalendarResource implements CalendarApi {
 
     private final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
     private static final Logger LOG = Logger.getLogger(CalendarResource.class.getName());
@@ -88,18 +91,113 @@ public class CalendarResource implements CalendarAPI {
 
     @Override
     public Response removeEvent(String tokenObjStr, String eventID) {
-        return null;
+        AuthToken tokenObj = g.fromJson(tokenObjStr, AuthToken.class); // Pode ser passado como TokenClass
+        LOG.fine("User: " + tokenObj.getUsername() + " is attempting to remove an event!");
+
+        Key tokenKey = KeyStore.tokenKeyFactory(tokenObj.getTokenID());
+
+        if (eventID.equals("")) {
+            LOG.warning("Empty event!");
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+
+        Transaction txn = datastore.newTransaction();
+
+        try {
+            Entity token = txn.get(tokenKey);
+
+            if (token == null) {
+                txn.rollback();
+                return Response.status(Response.Status.FORBIDDEN).build();
+            }
+
+            Key eventKey = KeyStore.calendarKeyFactory(eventID);
+
+            LOG.info("id: " + eventID);
+
+            txn.delete(eventKey);
+
+            txn.commit();
+
+            LOG.info("Event removed successfully with ID: " + eventID);
+            return Response.status(Response.Status.OK)
+                    .build();
+        } catch (Exception e) {
+            txn.rollback();
+            LOG.severe("An error occurred while adding event: " + e.getMessage());
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        } finally {
+            if (txn.isActive()) {
+                txn.rollback();
+            }
+        }
+
     }
 
     @Override
-    public Response editEvent(String tokenObjStr, String eventID) {
-        return null;
+    public Response editEvent(String tokenObjStr, Event event) {
+        AuthToken tokenObj = g.fromJson(tokenObjStr, AuthToken.class); // Pode ser passado como TokenClass
+        LOG.fine("User: " + tokenObj.getUsername() + " is attempting to edit an event!");
+
+        Key tokenKey = KeyStore.tokenKeyFactory(tokenObj.getTokenID());
+
+        LOG.info("event_title: " + event.getTitle());
+        LOG.info("event_description: " + event.getDescription());
+        LOG.info("event_date: " + event.getDate());
+        LOG.info("event_end_date: " + event.getEndDate());
+        LOG.info("username: " + tokenObj.getUsername());
+
+
+        if (event.getTitle().equals("")) {
+            LOG.warning("Empty event!");
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+
+        Transaction txn = datastore.newTransaction();
+
+        try {
+            Entity token = txn.get(tokenKey);
+
+            if (token == null) {
+                txn.rollback();
+                return Response.status(Response.Status.FORBIDDEN).build();
+            }
+
+            Key eventKey = KeyStore.calendarKeyFactory(event.getEventID());
+
+            LOG.info("id: " + event.getEventID());
+
+            Entity eventEntity = Entity.newBuilder(eventKey)
+                    .set("event_title", event.getTitle())
+                    .set("event_description", event.getDescription())
+                    .set("event_date", event.getDate())
+                    .set("event_end_date", event.getEndDate())
+                    .set("username", tokenObj.getUsername())
+                    .build();
+
+            EntityWithKey<Entity> entityWithKey = new EntityWithKey<>(eventKey, eventEntity);
+            txn.put(eventEntity);
+
+            txn.commit();
+
+            LOG.info("Event edited successfully with ID: " + event.getEventID());
+            return Response.status(Response.Status.OK)
+                    .entity(g.toJson(entityWithKey)).build();
+        } catch (Exception e) {
+            txn.rollback();
+            LOG.severe("An error occurred while adding event: " + e.getMessage());
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        } finally {
+            if (txn.isActive()) {
+                txn.rollback();
+            }
+        }
     }
 
     @Override
     public Response getEvent(String tokenObjStr, String eventID) {
         AuthToken tokenObj = g.fromJson(tokenObjStr, AuthToken.class); // Pode ser passado como TokenClass
-        LOG.fine("User: " + tokenObj.getUsername() + " is attempting to add an event!");
+        LOG.fine("User: " + tokenObj.getUsername() + " is attempting to get an event!");
 
         Key tokenKey = KeyStore.tokenKeyFactory(tokenObj.getTokenID());
 
@@ -143,6 +241,51 @@ public class CalendarResource implements CalendarAPI {
 
     @Override
     public Response getEvents(String tokenObjStr) {
-        return null;
+        AuthToken tokenObj = g.fromJson(tokenObjStr, AuthToken.class); // Pode ser passado como TokenClass
+        LOG.fine("User: " + tokenObj.getUsername() + " is attempting to get an event!");
+
+        Key tokenKey = KeyStore.tokenKeyFactory(tokenObj.getTokenID());
+
+
+        Transaction txn = datastore.newTransaction();
+
+        try {
+            Entity token = txn.get(tokenKey);
+
+            if (token == null) {
+                txn.rollback();
+                return Response.status(Response.Status.FORBIDDEN).build();
+            }
+
+            Query<Entity> query = Query.newEntityQueryBuilder()
+                    .setKind("Calendargit") // Replace "event" with the appropriate kind of your entity
+                    .setFilter(StructuredQuery.PropertyFilter.eq("username", tokenObj.getUsername()))
+                    .build();
+
+            QueryResults<Entity> results = datastore.run(query);
+
+            List<Entity> entities = new ArrayList<>();
+
+            while (results.hasNext()) {
+                Entity entity = results.next();
+                entities.add(entity);
+                LOG.info("Event: " + entity.getString("event_title"));
+            }
+
+
+            txn.commit();
+
+            LOG.info("Get all events successfully");
+            return Response.status(Response.Status.OK)
+                    .entity(g.toJson(entities)).build();
+        } catch (Exception e) {
+            txn.rollback();
+            LOG.severe("An error occurred while getting event: " + e.getMessage());
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        } finally {
+            if (txn.isActive()) {
+                txn.rollback();
+            }
+        }
     }
 }
