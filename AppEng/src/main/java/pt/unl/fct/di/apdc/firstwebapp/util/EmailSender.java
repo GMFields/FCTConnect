6f95e852d97f40a2efb6d0a26f7d62a62df50279
Afonso <ane.nunes@campus.fct.unl.pt>
2023-07-15 17:39:41 +0100

    Recuperação de palavra-passe através do email ja funciona

--- AppEng/src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/EmailSender.java
+++ AppEng/src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/EmailSender.java
@@ -11,11 +11,13 @@ public class EmailSender {
 
     private static final String SENDGRID_API_KEY = "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";
     private static final String ACCOUNT_ACTIVATION_MESSAGE= "Bem vindo à EduVersity. Por favor ativa a tua conta clicando no link: ";
-
     private static final String DOCENTE_WELCOME_MESSAGE= "Bem vindo à EduVersity. Por favor contacte um admnistrador para se autenticar e ativar a sua conta ";
-
+    private static final String RESET_MESSAGE = "Parece que recebemos um pedido para recuperar a tua password. Se não foste tu ignora este e-mail, caso contrário clica no link: ";
+    private static final String NEW_PASSWORD_MESSAGE = "A tua palavra passe foi alterada com sucesso. \n A tua nova password é: ";
+    private static final String NEW_PASSWORD_MESSAGE2 = "\nPor favor para mais segurança, muda a password assim que entrares.";
     private static final String FROM_EMAIL = "amddiscipulos@gmail.com";
     private static final String SUBJECT = "Ativação da conta";
+    private static final String SUBJECT_ONE = "Recuperação de passowrd";
 
     private static EmailSender instance;
     private EmailSender() {
@@ -40,7 +42,17 @@ public class EmailSender {
 
     }
 
+    public void sendResetPasswordEmail(String toEmail, String resetToken) throws IOException {
+        String resetLink = "https://helical-ascent-385614.oa.r.appspot.com/rest/users/reset?resetToken=" + resetToken;
+        String emailBody = RESET_MESSAGE+resetLink;
+        sendEmail(FROM_EMAIL, toEmail, SUBJECT_ONE, emailBody);
+    }
 
+    public void sendResetPasswordConfirmationEmail(String toEmail, String newPassword) throws IOException {
+        String emailBody = NEW_PASSWORD_MESSAGE+newPassword+NEW_PASSWORD_MESSAGE2;
+        sendEmail(FROM_EMAIL, toEmail, SUBJECT_ONE, emailBody);
+
+    }
     public void sendEmail(String fromEmail, String toEmail, String subject, String body) throws IOException {
         Email from = new Email(fromEmail);
         Email to = new Email(toEmail);
